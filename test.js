2c7f18b08221bce8332d892858c25f9cc2e85b07
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import cascadingKeyChain from "./source.js"
+import attach from "./source"
 
 test(({same, end}) => {
   same(
